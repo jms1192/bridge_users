654d7ceb17b5b2309079d873990867cd9f140915
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-31 20:28:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ def create_premade_layout(layout, data_link, type = ''):
 
 
 df = pd.DataFrame(
-    array([[2,6],[7,14]]),
+    ([[2,6],[7,14]]),
     columns=('col %d' % i for i in range(2)))
 
 st.dataframe(df)  # Same as st.write(df)
